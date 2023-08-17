bcb32aba50115bc3d85d81a5d4d45264570db176
Higor Santos <higor.stos@outlook.com>
2023-08-16 23:00:26 -0300

    criando a seção shows

---
+++ src/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(button) {
+            const target = button.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${target}]`);
+
+            removeActiveTab();
+            tab.classList.add('shows__list--is-active');
+            
+            removeActiveButton();
+            button.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+})
+
+function removeActiveTab() {
+    const tabs = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('shows__list--is-active');
+    }
+}
+
+function removeActiveButton() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
